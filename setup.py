c27df08a2a671b63b767de7b4b012d525782f97c
mm7490 <muellnermarco@gmail.com>
2018-03-21 15:38:35 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='PyMesaHandler',
-    version='0.0.1',
+    version='0.1.0',
     packages=['MesaHandler'],
     url='https://github.com/muma7490/PyMesaHandler',
     license='MIT',
