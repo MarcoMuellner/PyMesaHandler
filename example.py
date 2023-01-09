0189fa138eabc19ebb8f771699524d99c943008e
Anuj Gautam <45739450+gautam-404@users.noreply.github.com>
2023-01-09 16:08:19 +0530

    Update example.py

--- example.py
+++ example.py
@@ -5,8 +5,8 @@ from MesaHandler import MesaAccess
 work = ProjectOps()        ## Use ProjectOps("your_project") for a custom project name  
 work.create()              ## Use boolean arguments 'overwrite' and 'clean' to work on existing projects
 
-work.loadProjInlist("relative/path/to/inlist_custom_project")
-# work.loadPGstarInlist("relative/path/to/inlist_custion_pgstar")
+work.loadProjInlist("relative/path/to/inlist")
+# work.loadPGstarInlist("relative/path/to/inlist")
 
 object = MesaAccess()
 object["initial_mass"] = 5
